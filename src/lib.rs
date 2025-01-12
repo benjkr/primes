ebd6783887c51b2f9412e689e1ebfd46f73ef91b
benjkr <benjkr@gmail.com>
2025-01-12 21:53:02 +0200

    Implemented sieve

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,185 @@
 #![allow(dead_code)]
 
-pub struct Primes {
-    pub primes_found: Vec<u64>,
+use std::{cell::RefCell, collections::HashSet, rc::Rc};
+
+const BATCH_SIZE: u64 = 0b10000000000000000000;
+
+struct Batch {
     current: u64,
+    size: u64,
+    offset: u64,
+}
+
+impl Batch {
+    fn new(batch_size: u64) -> Self {
+        Self {
+            current: 0,
+            size: batch_size,
+            offset: 0,
+        }
+    }
+
+    fn calculate_offset(&mut self) {
+        self.offset = self.current * self.size;
+    }
+
+    fn set_batch(&mut self, batch: u64) {
+        self.current = batch;
+        self.calculate_offset();
+    }
+
+    fn update_batch(&mut self, f: fn(u64) -> u64) {
+        self.current = f(self.current);
+        self.calculate_offset();
+    }
+}
+
+pub struct Primes {
+    inner_slice: Box<Vec<bool>>,
+    pub primes_found: Rc<RefCell<HashSet<u64>>>,
+    pub primes_ordered: Vec<u64>,
+    batch: Batch,
 }
 
 impl Primes {
     pub fn new() -> Self {
-        Self {
-            primes_found: vec![],
-            current: 2,
+        Self::with_batch_size(BATCH_SIZE)
+    }
+
+    pub fn with_batch_size(batch_size: u64) -> Self {
+        let mut s = Self {
+            inner_slice: Box::new(vec![true; batch_size as usize]),
+            primes_found: Rc::new(RefCell::new(HashSet::new())),
+            primes_ordered: Vec::new(),
+            batch: Batch::new(batch_size),
+        };
+        s.populate_first_batch();
+        s.save_primes();
+
+        s
+    }
+
+    pub fn iter(&mut self) -> PrimesIterator<'_> {
+        PrimesIterator {
+            primes: self,
+            current: 0,
         }
     }
 
-    pub fn next_prime(&mut self) -> u64 {
-        while !self.is_prime(self.current) {
-            self.current += 1;
+    fn populate_first_batch(&mut self) {
+        self.inner_slice[0] = false;
+        self.inner_slice[1] = false;
+        for i in 2..self.inner_slice.len() {
+            if self.inner_slice[i] {
+                let mut tmp = i + i;
+                while tmp < self.batch.size as usize {
+                    self.inner_slice[tmp] = false;
+
+                    tmp += i;
+                }
+            }
         }
-        self.primes_found.push(self.current);
-        let prime = self.current;
+    }
+
+    fn save_primes(&mut self) {
+        let primes: Vec<u64> = self
+            .inner_slice
+            .iter()
+            .enumerate()
+            .filter_map(|(i, is_prime)| {
+                if *is_prime {
+                    Some(i as u64 + self.batch.offset)
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        self.primes_found.borrow_mut().extend(&primes);
+        self.primes_ordered.extend(&primes);
+    }
+
+    pub fn populate_next_batch(&mut self) {
+        self.batch.update_batch(|current| current + 1);
+        self.inner_slice.fill(true); // Reset the slice to all trues
+
+        for prime in self.primes_ordered.iter() {
+            let mul = (self.batch.offset as f64 / *prime as f64).ceil() as u64; // How much to multiply prime to reach offset (closest)
+            let mut tmp = prime * mul;
+
+            while tmp < self.batch.offset + self.batch.size {
+                let indx = tmp - self.batch.offset;
+                self.inner_slice[indx as usize] = false;
+
+                tmp += *prime;
+            }
+        }
+
+        self.save_primes();
+    }
+
+    pub fn is_prime(&mut self, n: u64) -> bool {
+        while n > (self.batch.current + 1) * BATCH_SIZE {
+            self.populate_next_batch();
+        }
+
+        self.primes_found.borrow().contains(&n)
+    }
+
+    pub fn primes_found_set(&self) -> Rc<RefCell<HashSet<u64>>> {
+        self.primes_found.clone()
+    }
+}
+
+pub struct PrimesIterator<'a> {
+    primes: &'a mut Primes,
+    current: usize,
+}
+
+impl Iterator for PrimesIterator<'_> {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.current >= self.primes.primes_ordered.len() {
+            self.primes.populate_next_batch();
+        }
+        let p = self.primes.primes_ordered[self.current];
         self.current += 1;
 
-        prime
-    }
-
-    pub fn has_prime(&self, p: u64) -> bool {
-        self.current > p && self.primes_found.binary_search(&p).is_ok()
-    }
-
-    fn is_prime(&mut self, n: u64) -> bool {
-        let s = (n as f64).sqrt() as u64;
-        for p in self.primes_found.iter() {
-            if *p > s {
-                break;
-            }
-            if n % p == 0 {
-                return false;
-            }
-        }
-        true
+        Some(p)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    const PRIMES: &'static str = include_str!("./test_static/primes.txt");
 
     #[test]
-    fn iter() {
-        let mut p = Primes::new();
-        let v: Vec<u64> = (0..5).map(|_| p.next_prime()).collect();
-        assert_eq!(v, [2, 3, 5, 7, 11]);
+    fn test_working() {
+        let mut p = Primes::with_batch_size(BATCH_SIZE);
+        assert!(p.is_prime(2));
+        assert!(p.is_prime(3));
+        assert!(!p.is_prime(4));
+        assert!(p.is_prime(5));
+        assert!(!p.is_prime(6));
+        assert!(p.is_prime(7));
+        assert!(p.is_prime(1299709));
+        assert!(!p.is_prime(1299708));
+    }
+
+    #[test]
+    fn test_file() {
+        let mut p = Primes::with_batch_size(BATCH_SIZE);
+
+        let primes_vec: Vec<u64> = PRIMES
+            .lines()
+            .map(|l| u64::from_str_radix(l.trim(), 10).unwrap())
+            .collect();
+
+        assert_eq!(
+            primes_vec,
+            p.iter().take(primes_vec.len()).collect::<Vec<u64>>()
+        )
     }
 }
